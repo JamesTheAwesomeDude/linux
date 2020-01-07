21cc65db151e6c5d55d0de30a088867cc5d22f6b
James <james.edington@uah.edu>
2020-01-07 13:21:38 -0600

    net: if_arp: clarify ARPHRD_IEEE1394 comment
    When used for networking via the in-tree firewire-net
    driver, IEEE 1394 devices will have hardware type=24
    regardless of whether IPv4 (IEEE RFC 2734) or IPv6
    (IEEE RFC 3146) is used.

--- include/uapi/linux/if_arp.h
+++ include/uapi/linux/if_arp.h
@@ -39,7 +39,7 @@
 #define ARPHRD_DLCI	15		/* Frame Relay DLCI		*/
 #define ARPHRD_ATM	19		/* ATM 				*/
 #define ARPHRD_METRICOM	23		/* Metricom STRIP (new IANA id)	*/
-#define	ARPHRD_IEEE1394	24		/* IEEE 1394 IPv4 - RFC 2734	*/
+#define	ARPHRD_IEEE1394	24		/* IEEE 1394 IP - RFC 2734/3146	*/
 #define ARPHRD_EUI64	27		/* EUI-64                       */
 #define ARPHRD_INFINIBAND 32		/* InfiniBand			*/
 
